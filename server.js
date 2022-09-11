a6dd12c762ea70da5aacf0ec93d22f99886a81ff
Jean Carlos Perez Santana <jean.perez@lever.co>
2022-09-11 17:19:55 -0400

    initial solution

---
+++ server.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const morgan = require('morgan')
+const connectDB = require('./db/connect')
+
+const bodyParser = require('body-parser')
+
+
+const { createUrl, findUrl } = require('./controllers/urlController')
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use(cors());
+app.use(morgan('tiny'))
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/shorturl/:code', findUrl)
+
+app.post('/api/shorturl', createUrl)
+
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server listening on port ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
